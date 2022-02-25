b6010e39e360c5f6a9f010e7a4eff26d4c1757d3
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2022-02-26 00:54:09 +0100

    config tailwind theme and logo assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans' : 'Poppins'
+    },
+    extend: {
+      fontFamily: {
+        "display" : 'Caveat'
+      },
+      colors: {
+        "acquamarine" : "#20E8AC",
+        "acquamarine-light" : "#8EE8CD",
+        "acquamarine-ultralight" : "#B7F7E4",
+        "paradise-pink": "#F52A67",
+        "duke-blue" : "#220694",
+        "jet" : "#3E3E3E"
+      },
+      rotate: {
+        "30": "30deg"
+      }
+    },
   },
   plugins: [],
 }
