d448446da28c1b8006cbd3a022865b47891da10a
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2022-02-26 00:55:12 +0100

    set up home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "assets/logo.png";
+import heart from "assets/heart.png";
 
-export default function Home() {
+const hearts = [
+  {id: 1, x: "inset-x-1/4", y: "bottom-20", rotate: "rotate-12", scale: "scale-110"},
+  {id: 2, x: "inset-x-3/4", y: "inset-y-3/4", rotate: "-rotate-12", scale: "scale-150"},
+  {id: 3, x: "left-12", y: "top-32", rotate: "rotate-30", scale: "scale-85"},
+  {id: 4, x: "right-12", y: "top-24", rotate: "-rotate-12", scale: "scale-50"},
+]
 
-  return (
-    <section>
-      <h1>Home</h1>
-      <p><Link to="/login">Login</Link></p>
+const Home = () => (
+    <section className="fixed inset-0 flex-center gap-3 flex-col py-72">
+      {hearts.map(({id, x, y, rotate, scale}) => 
+        <img key={id} className={`absolute ${x} ${y} ${rotate} ${scale}`}  alt="" src={heart} />
+      )}
+      <img alt="" src={logo} />
+      <h1 className="font-display text-4xl font-medium">Instalike</h1>
+      <span className="text-4xl mt-auto">👇🏼</span>
+      <button type="button" className="button-outlined"><Link className="text-gradient hover:text-white" to="/login">Join in</Link></button>
     </section>
   );
-}
+
+  export default Home;
