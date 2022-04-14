3167f521cb862bf68a1eda86f33d2cd474b9d916
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2022-04-14 12:20:25 +0200

    replace browerrouter with hashrouter for gh-pages compatibility

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "assets/style/index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import initStore from "redux/store";
 import App from "App";
 import "./socket";
@@ -12,9 +12,9 @@ const store = initStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
